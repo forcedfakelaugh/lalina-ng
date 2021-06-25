f882ddeb235940a4d7d7ab7d91daf5e774b62423
Son Nguyen <son.n@outlook.com>
2021-06-25 20:39:36 +0700

    Edit menu component

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -9,6 +9,7 @@ import { MenuItem } from 'src/app/models/menu-item.model';
 export class MenuComponent implements OnInit {
 
   menuItems: MenuItem[] = [];
+  selected!: MenuItem;
 
   constructor() { }
 
@@ -17,13 +18,19 @@ export class MenuComponent implements OnInit {
       {id: 1, name: "Pepperoni Pizza", price: "180,000 vnđ", photoUrl: "assets/img/img-pizza.jpg", description: "Delicious pizza"},
       {id: 2, name: "Amazing Panda", price: "120,000 vnđ", photoUrl: "assets/img/img-kids.jpg", description: "Delicious kids meal"},
       {id: 3, name: "Spaghetti", price: "130,000 vnđ", photoUrl: "assets/img/img-spaghetti.jpg", description: "Delicious dish"},
-      {id: 3, name: "Juices", price: "130,000 vnđ", photoUrl: "assets/img/juices.jpg", description: "Delicious juice"},
-      {id: 3, name: "Yoghurt", price: "70,000 vnđ", photoUrl: "assets/img/yoghurt.jpg", description: "Delicious yoghurt"},
-      {id: 3, name: "Ribs", price: "180,000 vnđ", photoUrl: "assets/img/ribs.jpg", description: "Delicious dish"},
-      {id: 3, name: "Sandwich", price: "130,000 vnđ", photoUrl: "assets/img/sw.jpg", description: "Delicious sandwich"},
-      {id: 3, name: "Orange Juice", price: "70,000 vnđ", photoUrl: "assets/img/oj.jpg", description: "Delicious juice"},
-      {id: 3, name: "Pistachio", price: "100,000 vnđ", photoUrl: "assets/img/img-pistachio.jpg", description: "Delicious snack"},
+      {id: 4, name: "Juices", price: "130,000 vnđ", photoUrl: "assets/img/juices.jpg", description: "Delicious juice"},
+      {id: 5, name: "Yoghurt", price: "70,000 vnđ", photoUrl: "assets/img/yoghurt.jpg", description: "Delicious yoghurt"},
+      {id: 6, name: "Ribs", price: "180,000 vnđ", photoUrl: "assets/img/ribs.jpg", description: "Delicious dish"},
+      {id: 7, name: "Sandwich", price: "130,000 vnđ", photoUrl: "assets/img/sw.jpg", description: "Delicious sandwich"},
+      {id: 8, name: "Orange Juice", price: "70,000 vnđ", photoUrl: "assets/img/oj.jpg", description: "Delicious juice"},
+      {id: 9, name: "Pistachio", price: "100,000 vnđ", photoUrl: "assets/img/img-pistachio.jpg", description: "Delicious snack"},
     ]
+
+    this.selected = this.menuItems[0]; // init
+  }
+
+  passDataToModal(id: number) {
+    this.selected = this.menuItems.filter(item => item.id === id)[0];
   }
 
 }
