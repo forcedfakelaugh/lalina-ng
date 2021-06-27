2c7f90aa999cf1e6f073b11021606085ee4c68c5
Son Nguyen <son.n@outlook.com>
2021-06-27 23:20:54 +0700

    improvement

--- src/app/components/footer-bar/footer-bar.component.ts
+++ src/app/components/footer-bar/footer-bar.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterBarComponent implements OnInit {
 
+  currentYear = new Date().getFullYear();
   constructor() { }
 
   ngOnInit(): void {
